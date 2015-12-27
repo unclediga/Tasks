6557bcbff4ced80ceeceef02467441bbc8132f2f
homeuser <zowie_i@mail.ru>
2015-12-28 00:36:37 +0300

    MindMaps

---
+++ MindMap/Зарплата.mm
@@ -0,0 +1,165 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0033ff" CREATED="1451155134468" HGAP="-37" ID="ID_1955050659" MODIFIED="1451158869234" TEXT="&#x417;&#x410;&#x420;&#x41f;&#x41b;&#x410;&#x422;&#x410;" VSHIFT="-79">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Arial Black" SIZE="18"/>
+<node COLOR="#338800" CREATED="1451155334031" HGAP="17" ID="ID_1796125670" MODIFIED="1451158845359" POSITION="left" TEXT="&#x411;&#x443;&#x445;&#x433;&#x430;&#x43b;&#x442;&#x435;&#x440;&#x438;&#x44f;" VSHIFT="-94">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial Black" SIZE="16"/>
+<node CREATED="1451157058515" ID="ID_1097458412" MODIFIED="1451159877109" TEXT="&#x412;&#x41e;&#x41f;&#x420;&#x41e;&#x421;&#x42b;">
+<cloud COLOR="#ffcccc"/>
+<arrowlink DESTINATION="ID_1097458412" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_792544664" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1097458412" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1358170144" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1097458412" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_792544664" SOURCE="ID_1097458412" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1097458412" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1358170144" SOURCE="ID_1097458412" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="yes"/>
+<icon BUILTIN="clanbomber"/>
+<node COLOR="#ff0000" CREATED="1451155494453" ID="ID_553102294" MODIFIED="1451159840375" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#ff3333">&#1054;&#1087;&#1083;&#1072;&#1090;&#1072; &#1074; &#1084;&#1086;&#1084;&#1077;&#1085;&#1090; &#1085;&#1077;&#1103;&#1074;&#1082;&#1080;?&#160; </font>
+    </p>
+    <p>
+      <font color="#ff3333">- &#1054;&#1090;&#1087;&#1091;&#1089;&#1082;? </font>
+    </p>
+    <p>
+      <font color="#ff3333">- &#1041;&#1086;&#1083;&#1100;&#1085;&#1080;&#1095;&#1085;&#1099;&#1081;? </font>
+    </p>
+    <p>
+      <font color="#ff3333">- &#1055;&#1086;&#1089;&#1086;&#1073;&#1080;&#1103;?</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node COLOR="#ff0000" CREATED="1451159313578" ID="ID_673224800" MODIFIED="1451159840968" STYLE="bubble" TEXT="&#x41f;&#x43e;&#x434;&#x43e;&#x439;&#x434;&#x451;&#x442; &#x43b;&#x438; &#x437;&#x430;&#x433;&#x440;&#x443;&#x437;&#x43a;&#x430; &#x420;&#x412;&#x41f;  ">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node COLOR="#ff0000" CREATED="1451155403562" ID="ID_1313938336" MODIFIED="1451159841421" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1056;&#1072;&#1089;&#1095;&#1105;&#1090; &#1086;&#1090;&#1087;&#1091;&#1089;&#1082;&#1072; VIP&#160;&#160;
+    </p>
+    <p>
+      &#1059;&#1064;&#1051;&#1054; &#1074; &#1088;&#1080;&#1089;&#1082;&#1080;???
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1451160335921" ID="ID_454704027" MODIFIED="1451160982031" TEXT="&#x41f;&#x440;&#x43e;&#x432;&#x43e;&#x434;&#x43a;&#x438;">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node COLOR="#338800" CREATED="1451155323984" ID="ID_949675328" MODIFIED="1451159219656" STYLE="bubble" TEXT="&#x417;&#x430;&#x43c;&#x435;&#x43d;&#x430; &#x441;&#x447;&#x435;&#x442;&#x43e;&#x432;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node COLOR="#338800" CREATED="1451155435453" ID="ID_1950174338" MODIFIED="1451159221468" STYLE="bubble" TEXT="&#x414;&#x432;&#x43e;&#x439;&#x43d;&#x44b;&#x435; &#x43f;&#x440;&#x43e;&#x432;&#x43e;&#x434;&#x43a;&#x438;">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node COLOR="#338800" CREATED="1451160367281" ID="ID_769403912" MODIFIED="1451160983390" TEXT="&#x41a;&#x412;&#x410;&#x420;&#x422;&#x410;&#x41b;">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#338800" CREATED="1451155442609" ID="ID_1457488172" MODIFIED="1451159223250" STYLE="bubble" TEXT="&#x420;&#x435;&#x437;&#x435;&#x440;&#x432; &#x43f;&#x43e; &#x43e;&#x442;&#x43f;&#x443;&#x441;&#x43a;&#x430;&#x43c;">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</node>
+<node CREATED="1451155909484" ID="ID_723101837" MODIFIED="1451158894500" POSITION="right" TEXT="&#x411;&#x41e;&#x421;&#x421;">
+<cloud COLOR="#ccffff"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="ksmiletris"/>
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node COLOR="#999999" CREATED="1451155138500" ID="ID_1637046999" MODIFIED="1451160540453" POSITION="left" TEXT="BOSPS">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font BOLD="true" NAME="Arial Black" SIZE="14"/>
+<node COLOR="#111111" CREATED="1451155147234" ID="ID_305401992" MODIFIED="1451155553656">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1057;&#1087;&#1088;&#1072;&#1074;&#1086;&#1095;&#1085;&#1080;&#1082;
+    </p>
+    <p>
+      &quot;&#1057;&#1095;&#1077;&#1090;&#1072;&quot;
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1451155227031" ID="ID_272838989" MODIFIED="1451161082812">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1053;&#1077;&#1089;&#1082;&#1086;&#1083;&#1100;&#1082;&#1086; &#1087;&#1088;&#1086;&#1074;&#1086;&#1076;&#1086;&#1082;
+    </p>
+    <p>
+      &#1085;&#1072; &#1086;&#1076;&#1085;&#1091; &#1080;&#1079; &#1041;&#1054;&#1057;&#1057;&#1072;
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1451160667156" ID="ID_1596819860" MODIFIED="1451162759796">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1055;&#1077;&#1088;&#1077;&#1076;&#1077;&#1083;&#1072;&#1090;&#1100; &#1092;&#1086;&#1088;&#1084;&#1091;
+    </p>
+    <p>
+      &#1085;&#1072; &#1080;&#1089;&#1087;&#1086;&#1083;&#1100;&#1079;&#1086;&#1074;&#1072;&#1085;&#1080;&#1077; &#1088;&#1072;&#1079;&#1085;&#1099;&#1093; &#1074;&#1077;&#1088;&#1089;&#1080;&#1081;
+    </p>
+    <p>
+      &#1087;&#1083;&#1072;&#1085;&#1072; &#1089;&#1095;&#1077;&#1090;&#1086;&#1074;.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1451160836921" ID="ID_426389385" MODIFIED="1451160946656">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#1052;&#1086;&#1078;&#1077;&#1090; &#1089;&#1076;&#1077;&#1083;&#1072;&#1090;&#1100; &#1086;&#1073;&#1098;&#1077;&#1082;&#1090;
+    </p>
+    <p>
+      &#1055;&#1051;&#1040;&#1053; &#1057;&#1095;&#1077;&#1090;&#1086;&#1074;?
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="korn"/>
+</node>
+</node>
+</node>
+<node CREATED="1451158288000" ID="ID_1496597011" MODIFIED="1451158302140" TEXT="&#x417;&#x430;&#x433;&#x440;&#x443;&#x437;&#x43a;&#x430; &#x438;&#x437; &#x411;&#x41e;&#x421;&#x421;&#x430;"/>
+</node>
+</node>
+</map>
